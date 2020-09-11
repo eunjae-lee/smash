f77577b4a348e44ba75236b077b9570556bcbdde
eunjae-lee <karis612@gmail.com>
2020-09-11 17:49:27 +0200

    add weekday

--- src/List.js
+++ src/List.js
@@ -31,6 +31,7 @@ const dateFormatter = new Intl.DateTimeFormat("default", {
   year: "numeric",
   month: "short",
   day: "numeric",
+  weekday: "short",
 });
 
 const reorder = (list, startIndex, endIndex) => {
@@ -81,25 +82,26 @@ export function List() {
 
   return (
     <div className="py-10 px-20">
-      <div className="p-2">
+      <div>
+        <button
+          type="button"
+          className="border-2 rounded-md bg-orange-600 hover:bg-orange-700 text-orange-200 w-full py-4 text-3xl"
+          onClick={() => {
+            history.push("/smash");
+          }}
+          disabled={list === null || list.length === 0}
+        >
+          Start Smashing!
+        </button>
+      </div>
+
+      <div className="p-2 mt-10">
         <p className="uppercase">
           <span className="text-orange-900 text-xl">Today</span>
           <span className="text-gray-700 text-sm ml-2">
             ({dateFormatter.format(new Date())})
           </span>
         </p>
-        <div className="mt-10">
-          <button
-            type="button"
-            className="border-2 rounded-md bg-orange-600 hover:bg-orange-700 text-orange-200 w-full py-4 text-3xl"
-            onClick={() => {
-              history.push("/smash");
-            }}
-            disabled={list === null || list.length === 0}
-          >
-            Start Smashing!
-          </button>
-        </div>
 
         <DragDropContext onDragEnd={onDragEnd}>
           <Droppable droppableId="droppable">
@@ -109,7 +111,7 @@ export function List() {
                 ref={provided.innerRef}
                 className={`${
                   snapshot.isDraggingOver ? "list-none" : "list-disc"
-                } mt-10`}
+                } mt-4`}
               >
                 {tasksToDo.map((item, index) => (
                   <Draggable key={item.id} draggableId={item.id} index={index}>
@@ -136,7 +138,7 @@ export function List() {
           </Droppable>
         </DragDropContext>
       </div>
-      <div className="mt-6">
+      <div>
         <form onSubmit={onSubmit}>
           <input
             type="text"
@@ -148,19 +150,21 @@ export function List() {
         </form>
       </div>
 
-      <p className="mt-10 text-gray-700 uppercase">Done today</p>
-      <ul className="mt-4">
-        {tasksDone.map((item, index) => (
-          <li className="mb-2 line-through text-gray-500">
-            <span className="text-2xl text-gray-500">{item.title}</span>
-            <span className="ml-4 text-gray-500">
-              {secondsToString(getSecondsSpent(item))}
-            </span>
-          </li>
-        ))}
-      </ul>
+      <div className="ml-2 mt-16">
+        <p className="text-gray-700 uppercase">Done today</p>
+        <ul className="mt-4">
+          {tasksDone.map((item, index) => (
+            <li className="mb-2 line-through text-gray-500" key={index}>
+              <span className="text-2xl text-gray-500">{item.title}</span>
+              <span className="ml-4 text-gray-500">
+                {secondsToString(getSecondsSpent(item))}
+              </span>
+            </li>
+          ))}
+        </ul>
+      </div>
 
-      <div className="p-2 mt-20">
+      <div className="p-2 mt-10">
         {Object.keys(oldTasksPerDay)
           .sort((a, b) => (a < b ? 1 : -1))
           .map((day) => {
@@ -168,7 +172,7 @@ export function List() {
             const title = dateFormatter.format(date);
 
             return (
-              <div key={title}>
+              <div key={title} className="mt-10">
                 <p className="uppercase text-gray-900">{title}</p>
                 <ul className="list-disc mt-4">
                   {oldTasksPerDay[day].map((task, index) => (
